5e91fd91594f774c01834c65c2f1d478a1e0febd
jangjh123 <gn0001231@gmail.com>
2024-01-19 00:13:31 +0900

    [CT-49] Coroutine 관련 DI 구현

---
+++ presentation/src/main/java/team/godsaeng/cooktionary_android/di/CoroutineDispatcherModule.kt
@@ -0,0 +1,24 @@
+package team.godsaeng.cooktionary_android.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+@Module
+@InstallIn(SingletonComponent::class)
+object CoroutineDispatcherModule {
+    @Provides
+    @DefaultDispatcher
+    fun provideDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
+
+    @Provides
+    @IoDispatcher
+    fun provideIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @Provides
+    @MainImmediateDispatcher
+    fun provideMainImmediateDispatcher(): CoroutineDispatcher = Dispatchers.Main.immediate
+}
---
+++ presentation/src/main/java/team/godsaeng/cooktionary_android/di/CoroutineDispatcherQualifiers.kt
@@ -0,0 +1,15 @@
+package team.godsaeng.cooktionary_android.di
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class DefaultDispatcher
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class IoDispatcher
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class MainImmediateDispatcher
