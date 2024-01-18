c4573bd22cbeac08ab6184a02f0203b506b09da1
jangjh123 <gn0001231@gmail.com>
2024-01-19 00:05:08 +0900

    [CT-49] 모듈 추가

---
+++ presentation/src/main/java/team/godsaeng/cooktionary_android/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package team.godsaeng.cooktionary_android.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PointColor = Color(0xFFFF5B29)
+val SubColor = Color(0xFF27262E)
+val ButtonBorderColor = Color(0xFF000000).copy(alpha = 0.03f)
+val IngredientButtonColor = Color(0xFFF3F5F9)
+val ProgressBackgroundColor = Color(0xFFF3F5F9)
+val TextColor = Color(0xFF27262E)
+val TextColorGrey1 = Color(0xFF434343)
+val TextColorGrey2 = Color(0xFFAAAAAA)
+val TextColorGrey3 = Color(0xFF949494)
+val RecipeDetailTextColor = Color(0xFF6B6B6B)
+val ImagePlaceHolderColor = Color(0xFFD9D9D9)
---
+++ presentation/src/main/java/team/godsaeng/cooktionary_android/ui/theme/Type.kt
@@ -0,0 +1,49 @@
+package team.godsaeng.cooktionary_android.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val Typography = Typography(
+    titleLarge = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.SemiBold,
+        letterSpacing = 0.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.SemiBold,
+        letterSpacing = 0.sp
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.Medium,
+        letterSpacing = 0.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.Normal,
+        letterSpacing = 0.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.Normal,
+        letterSpacing = 0.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.SemiBold,
+        letterSpacing = 0.sp,
+    ),
+    labelMedium = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.Normal,
+        letterSpacing = 0.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = pretendard,
+        fontWeight = FontWeight.Thin,
+        letterSpacing = 0.sp
+    )
+)
