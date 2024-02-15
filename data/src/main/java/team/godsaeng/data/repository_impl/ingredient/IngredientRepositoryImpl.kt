c64089ead8d7e8e63bd93e7d7046492fab0d8af5
jangjh123 <gn0001231@gmail.com>
2024-02-16 01:54:00 +0900

    [CT-86] 재료 버튼 불러오기 기능 구현
    비회원의 경우 추후 처리 예정

--- data/src/main/java/team/godsaeng/data/repository_impl/ingredient/IngredientRepositoryImpl.kt
+++ data/src/main/java/team/godsaeng/data/repository_impl/ingredient/IngredientRepositoryImpl.kt
@@ -6,15 +6,22 @@ import team.godsaeng.domain.model.model.CTException
 import team.godsaeng.domain.model.model.ResponseState
 import team.godsaeng.domain.model.model.ingredient.Ingredient
 import team.godsaeng.domain.model.repository.ingredient.IngredientRepository
-import java.io.IOException
 import javax.inject.Inject
 
 class IngredientRepositoryImpl @Inject constructor(private val cooktionaryApi: CooktionaryApi) : IngredientRepository {
     override suspend fun fetchIngredient(name: String): ResponseState<Ingredient> {
         return try {
             val response = cooktionaryApi.getIngredient(name)
-            IOException("asd")
             ResponseState.OnSuccess(response.data.toDomainModel())
+        } catch (exception: CTException) {
+            ResponseState.OnFailure(exception.ctError)
+        }
+    }
+
+    override suspend fun fetchMyIngredientList(): ResponseState<List<Ingredient>> {
+        return try {
+            val response = cooktionaryApi.getMyIngredientList()
+            ResponseState.OnSuccess(response.data.map { it.toDomainModel() })
         } catch (exception: CTException) {
             ResponseState.OnFailure(exception.ctError)
         }
